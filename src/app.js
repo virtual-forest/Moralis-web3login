e89a0cb8520a2be810a67f4cf22fa4c4e460b593
Harsh.eth <harshkumarjadon@gmail.com>
2023-09-24 11:03:15 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,7 +7,34 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const router = require("./routes/user.routes");
+app.use(cors({ origin: process.env.REACT_URL, credentials: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
+//common routes
+app.use("/", router);
+
+app.use("/home", (req, res) => {
+  res.send("This is testing Home page")
+})
+
+const startServer = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_CONNECTION);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
+
+  try {
+    await Moralis.start({
+      apiKey: process.env.MORALIS_API_KEY,
+    });
+    console.log(`Connected with Moralis`);
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
 
 
   const port = process.env.PORT;
